4e4c6b8251721542e2117666e32d3c83045ffec4
Matteo Vaccari <vaccari@pobox.com>
2024-09-08 11:16:31 +0200

    First test passes: fail on write failure

--- main_test.go
+++ main_test.go
@@ -2,46 +2,25 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
-
-//func Test_serve(t *testing.T) {
-//	type args struct {
-//		conn io.ReadWriter
-//	}
-//	tests := []struct {
-//		name string
-//		args args
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, test := range tests {
-//		t.Run(test.name, func(t *testing.T) {
-//			serve(test.args.conn)
-//		})
-//	}
-//}
 
 type failWriter struct {
 }
 
+var writeFailed = fmt.Errorf("write failed")
+
 func (f *failWriter) Write(p []byte) (n int, err error) {
-	return 0, fmt.Errorf("write failed")
+	return 0, writeFailed
 }
 
 func (f *failWriter) Read(p []byte) (n int, err error) {
 	return 0, nil
 }
 
-func test_serve_returns_error_on_read_error(t *testing.T) {
+func Test_serve_returns_error_on_write_error(t *testing.T) {
 	failWriter := &failWriter{}
-	var conn io.ReadWriter = failWriter
-	buf := make([]byte, maxPayload)
-	for {
-		if err := interact(conn, buf); err != nil {
-			fmt.Println("Error from connection:", err)
-			break
-		}
-	}
+	err := interact(failWriter, make([]byte, maxPayload))
+	assert.ErrorIs(t, err, writeFailed)
 }
